952448a33744c80addc6d197654fc2852f7ba628
momomox <moroto012@gmail.com>
2018-05-04 18:32:58 +0900

    Modify increment2#

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%", current_user.id)
-	respond_to do |format|
-	format.html
-	format.json
-	end
-end
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-if current_user.update(user_params)
-redirect_to root_path
-else
-render :edit
-end
-end
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
-private
+  private
 
-def user_params
-params.require(:user).permit(:name, :email)
-end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
