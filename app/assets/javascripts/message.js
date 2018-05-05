6ffaed3ea25b405c5457e5562f713a18e00ee7ce
momomox <moroto012@gmail.com>
2018-05-05 15:35:57 +0900

    Modify auto updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,57 +1,59 @@
 $(document).on('turbolinks:load', function(){
 	$(function(){
-	  function buildHTML(message){
-	  	message.image ? image = `<img src=${message.image}>` : image = "";
-	  	var html = `<li class="message" msg-id = "${message.id}">
-		  						<h2>${message.user_name}</h2>
+		function buildHTML(message){
+			message.image ? image = `<img src=${message.image}>` : image = "";
+			var html = `<li class="message" msg-id = "${message.id}">
+									<h2>${message.user_name}</h2>
 									<h4>${message.date}</h4>
 									<p>${message.content}</p>
 									${image}
 									</li>`
 			return html;
-	  }
-	  $('#new_message').on('submit', function(e){
-	    e.preventDefault();
-	    var formData = new FormData(this);
-	    var url = $(this).attr('action');
-	    $.ajax({
-	      url: url,
-	      type: "POST",
-	      data: formData,
-	      dataType: 'json',
-	      processData: false,
-	      contentType: false
-	    })
-	   .done(function(data){
-	   	 var html = buildHTML(data);
-	   	 $('.main-contents__middle__message').append(html)
-	   	 $('#new_message')[0].reset();
-       $('.main-contents__middle').animate({scrollTop: $('.main-contents__middle')[0].scrollHeight}, 'swing');
-       $('.send_button').prop("disabled", false);
-	   })
-	   .fail(function(){
-	   	 alert('error');
-	   })
+		}
+		$('#new_message').on('submit', function(e){
+		  e.preventDefault();
+		  var formData = new FormData(this);
+		  var url = $(this).attr('action');
+		  $.ajax({
+				url: url,
+				type: "POST",
+				data: formData,
+				dataType: 'json',
+				processData: false,
+				contentType: false
+			})
+			.done(function(data){
+				var html = buildHTML(data);
+				$('.main-contents__middle__message').append(html)
+				$('#new_message')[0].reset();
+				$('.main-contents__middle').animate({scrollTop: $('.main-contents__middle')[0].scrollHeight}, 'swing');
+				$('.send_button').prop("disabled", false);
+			})
+			.fail(function(){
+				alert('error');
+			})
 	  })
 		function getMsg() {
-    var newMsgId = $('.message').last().attr('msg-id')
-    console.log(newMsgId);
-    var url = $('#new_message').attr('action');
-    $.ajax ({
-      type: 'GET',
-      url: url,
-      data: { msg_id: newMsgId },
-      dataType: 'json'
-    })
-    .done(function(data){
-      if (data.length == 0) return false
-      data.forEach(function(msg) {
-        var html = buildHTML(msg)
-        $('.main-contents__middle__message').append(html)
-      })
-      $('.main-contents__middle').animate({ scrollTop: $('.main-contents__middle')[0].scrollHeight}, 'swing')
-    })
-  }
-  setInterval(getMsg, 5000)
+			var newMsgId = $('.message').last().attr('msg-id');
+			var url = $('#new_message').attr('action');
+			console.log(location.pathname);
+			$.ajax ({
+				type: 'GET',
+				url: url,
+				data: { msg_id: newMsgId },
+				dataType: 'json'
+			})
+			.done(function(data){
+			if (data.length == 0) return false
+			data.forEach(function(msg) {
+			var html = buildHTML(msg)
+			$('.main-contents__middle__message').append(html)
+			})
+			$('.main-contents__middle').animate({ scrollTop: $('.main-contents__middle')[0].scrollHeight}, 'swing')
+			})
+		}
+		if (location.pathname.match(/messages/)){
+			setInterval(getMsg, 5000)
+		}
 	})
 })
